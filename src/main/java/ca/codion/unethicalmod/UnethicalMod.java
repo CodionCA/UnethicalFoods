2d64dea3f7ad57bbd7273612e5c1b1855be31f4c
CodionCA <codiontl@Outlook.com>
2023-02-14 22:17:33 -0500

    Remove Comment

--- src/main/java/ca/codion/unethicalmod/UnethicalMod.java
+++ src/main/java/ca/codion/unethicalmod/UnethicalMod.java
@@ -12,7 +12,6 @@ public class UnethicalMod implements ModInitializer {
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
 	}
